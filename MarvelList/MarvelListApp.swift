b3e56623b680bc0775918ceeb5ea3d2ef2c8879f
George Davies <george.davies@revolut.com>
2022-10-22 07:34:42 +0100

    Create project

---
+++ MarvelList/MarvelListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelListApp.swift
+//  MarvelList
+//
+//  Created by George Davies on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MarvelListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
